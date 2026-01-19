a42e1cb1ea6e46fc31cbc8a62a3fcf641d0e29b2
Tom François <tom.francois@ntymail.com>
2026-01-19 11:51:01 +0100

    fix: wrong project url in the report generation

--- src/sw_metadata_bot/pitfalls.py
+++ src/sw_metadata_bot/pitfalls.py
@@ -89,7 +89,7 @@ If you're not interested in participating, you can comment "unsubscribe" and we'
 {report}
 ---
 
-This report was generated automatically by [sw-metadata-bot](https://github.com/codemetasoft/sw-metadata-bot).
+This report was generated automatically by [sw-metadata-bot](https://github.com/SoftwareUnderstanding/sw-metadata-bot).
 """
 
 
